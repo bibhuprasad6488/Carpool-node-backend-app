6426978026b90085571f9d134127c3825f675bab
Rudranarayan <rudranarayan.maastrix@gmail.com>
2026-07-22 18:19:57 +0530

    Updated API:

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(cors({
     origin: [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://192.168.1.4:3000"
+        "http://192.168.1.4:3000",
+        "https://carpooling-fe.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
