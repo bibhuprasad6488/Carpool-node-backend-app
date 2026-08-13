ed343f60cf30afd77c96bfd2d92c6acdeb0b9812
Bibhu Prasad <119917682+bibhuprasad6488@users.noreply.github.com>
2026-08-13 17:31:56 +0530

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -127,7 +127,7 @@ exports.register = async (req, res) => {
     await connection.beginTransaction();
 
     // 1. Create Base User
-    // await connection.query("SET time_zone = '+05:30'");
+    await connection.query("SET time_zone = '+05:30'");
 
     const [userResult] = await connection.query(
       `INSERT INTO users (name, email, phone, password, role, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(), NOW())`,
