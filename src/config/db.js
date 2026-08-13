318cf71c3ed2e414eac849688f810167f3c57ca6
Bibhu Prasad <119917682+bibhuprasad6488@users.noreply.github.com>
2026-08-13 17:00:06 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,30 +1,33 @@
 const mysql = require('mysql2/promise');
 
-if (process.env.NODE_ENV == 'local') {
+const dbConfig = {
+    waitForConnections: true,
+    connectionLimit: 10,
+    timezone: '+05:30'
+};
+
+if (process.env.NODE_ENV === 'local') {
     var pool = mysql.createPool({
+        ...dbConfig,
         host: 'localhost',
         port: 3306,
         user: 'root',
         password: '',
-        database: 'carpool_nest',
-        waitForConnections: true,
-        connectionLimit: 10
+        database: 'carpool_nest'
     });
 } else {
     pool = mysql.createPool({
+        ...dbConfig,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10,
         connectTimeout: 10000,
         ssl: {
             rejectUnauthorized: false
         }
     });
 }
-
 
 module.exports = pool;
